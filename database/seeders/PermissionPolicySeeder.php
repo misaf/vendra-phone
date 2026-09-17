5ecf7c4424854c375e5b81f9e292611cd33779f5
misaf <misaf.1990@gmail.com>
2026-09-17 18:30:52 +0000

    refactor(verification): tidy seeder docblocks and tenant lookups

--- database/seeders/PermissionPolicySeeder.php
+++ database/seeders/PermissionPolicySeeder.php
@@ -11,7 +11,9 @@ final class PermissionPolicySeeder extends BasePermissionPolicySeeder
 {
     protected const string MODULE_NAME = 'vendra-phone';
 
-    /** @return list<string> */
+    /**
+     * @return list<string>
+     */
     protected function policies(): array
     {
         return array_column(PhoneNumberPolicyEnum::cases(), 'value');
