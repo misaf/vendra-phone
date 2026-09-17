cf0e7dff06b83918ee703d4a9da7a0378216f95b
misaf <misaf.1990@gmail.com>
2026-09-17 19:49:32 +0000

    test(store): cover the storefront:lifecycle command

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -10,9 +10,9 @@ use Misaf\VendraPhone\Database\Seeders\PermissionPolicySeeder;
 use Misaf\VendraSupport\Tenancy\Console\Commands\TenantSeedCommand;
 
 #[Description('Seed phone module data for a tenant')]
-#[Signature('vendra-phone:seed
-        {tenant? : Tenant ID or slug to seed phone permissions for}
-        {seeders?* : Seeder keys to run. Use "all" or: permission-policies}')]
+#[Signature(self::MODULE_NAME.':seed
+        {tenant? : Tenant ID or slug to seed phone data for}
+        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies}')]
 final class SeedCommand extends TenantSeedCommand
 {
     protected const string MODULE_NAME = 'vendra-phone';
@@ -22,6 +22,8 @@ final class SeedCommand extends TenantSeedCommand
      */
     protected function seeders(): array
     {
-        return ['permission-policies' => PermissionPolicySeeder::class];
+        return [
+            'permission-policies' => PermissionPolicySeeder::class,
+        ];
     }
 }
